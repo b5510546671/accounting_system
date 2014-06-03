a6e51a736dbb29b5338c11c4c33e28a6cefd86e6
Supavit Kongwudhikunakorn <supavit_knot@yahoo.co.th>
2014-06-03 16:09:41 +0700

    added home.php and fix auth.php

--- src/auth.php
+++ src/auth.php
@@ -2,8 +2,8 @@
     //Start session
     session_start();
     //Check whether the session variable SESS_USERNAME is present or not
-    if(!isset($_SESSION['SESS_USERNAME']) || (trim($_SESSION['SESS_USERNAME'] == '')){
+    if(!isset($_SESSION['SESS_USERNAME']) || (trim($_SESSION['SESS_USERNAME'] == ''))){
         header("location: index.php");
         exit();
     }
->
+?>
